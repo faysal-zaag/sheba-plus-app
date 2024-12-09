719dbb7d2a86c9003c2e864cc764d14c1cd5d6d5
mahabubshozib <mahabubshozib.os@gmail.com>
2024-12-09 12:52:40 +0600

    add display center repository

---
+++ lib/view_model/repositories/display_center_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+/**
+ * Created by mahabubshozib on 12/9/24.
+ * Author: Md Mahabub Hossain Shozib
+ */
+
+class DisplayCenterServiceRepository {
+  final Dio _dio;
+  DisplayCenterServiceRepository(this._dio);
+
+
+}
